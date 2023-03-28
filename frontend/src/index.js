19a27929c12ac7a366864b27063fb4761f051db8
gigihdwirahmadi <gigihdwirahmadi@gmail.com>
2023-03-28 20:04:36 +0700

    first-commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import Register from './pages/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/register"
+          element={<Register />}
+          />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
